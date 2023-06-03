a41a1c046da4a8448a9d0983dfa5de676cbc075d
JiangShi-Github <caiyuchen319@163.com>
2023-06-03 18:31:01 +0800

    更改Snake

--- src/main.cpp
+++ src/main.cpp
@@ -20,24 +20,24 @@ int main()
 {
     // std::cout << "Hello World" << std::endl;
     
-   s = new Screen("/dev/fb0");
+    s = new Screen("/dev/fb0");
     s->clear();
 
 
-   g = new Ground(640, 480, 5, 5, 0xFF0000, 0xFFFFFF, 0xD2D2D0);
+    g = new Ground(640, 480, 5, 5, 0xFF0000, 0xFFFFFF, 0xD2D2D0);
     g->draw(s, 0, 0);
 
-   
 
-   
-   sn = new Snake(g, 0xB20101);
+
+
+    sn = new Snake(g, 0xB20101);
 
     k = new Keeper();
     dev1 = new InputDev(sn, "/dev/input/event0");
 
     while (1)
     {
-
+        
     }
 
     delete sn;
